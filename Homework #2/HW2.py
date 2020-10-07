90184f0ca8db24600d21ba193d7a14f61f7cdc0e
tran-rickydragon <70486087+tran-rickydragon@users.noreply.github.com>
2020-10-07 02:04:46 -0500

    Section B

--- Homework #2/HW2.py
+++ Homework #2/HW2.py
@@ -1,8 +1,5 @@
 """Ricky Tran - 1832920"""
 
-"""start off with an empty date list"""
-dates = []
-
 """List of months for verifying correct format"""
 
 month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
@@ -11,28 +8,18 @@ month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August
 
 months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
           'September': 9, 'October': 10, 'November': 11, 'December': 12}
+input_dates = open(r'C:\Users\ricky\OneDrive\Desktop\School\20\Fall_20\CIS2348\inputDates.txt')
 
-"""Loop for verifying if the input is the correct format and then storing it in the dates list"""
-date = 0
-while date != '-1':
-    date = input()
-    form = date.split(' ')
-    if form[0] in month:
-        if ',' in form[1]:
-            if len(form[2]) == 4:
-                dates.append(date)
+"""Modified the loop so that the input is taken from the file"""
 
-"""Loop for printing dates from the dates list with the new format"""
-i = 0
-j = int(len(dates))
-
-while i < j:
-    x = dates[i].split(' ')
-    a = x[0]
-    b = x[1]
-    c = x[2]
-    a = months[a]
-    b = b.replace(',', '')
-    i += 1
-    new_format = '{}/{}/{}'
-    print(new_format.format(a, b, c))
+for date in input_dates:
+    x = date.split(' ')
+    if x[0] in month:
+        if ',' in date:
+                a = x[0]
+                b = x[1]
+                c = x[2]
+                a = months[a]
+                b = b.replace(',', '')
+                new_format = '{}/{}/{}'
+                print(new_format.format(a, b, c))
