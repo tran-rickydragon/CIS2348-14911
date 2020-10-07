9ac75793896595b195c990b2f121ec1807911562
tran-rickydragon <70486087+tran-rickydragon@users.noreply.github.com>
2020-10-07 02:54:51 -0500

    Add files via upload

---
+++ Homework #2/6.17.py
@@ -0,0 +1,17 @@
+password = str(input(''))
+
+for x in password:
+    if x == 'i':
+        password = password.replace('i', '!')
+    elif x == 'a':
+        password = password.replace('a', '@')
+    elif x == 'm':
+        password = password.replace('m', 'M')
+    elif x == 'B':
+        password = password.replace('B', '8')
+    elif x == 'o':
+        password = password.replace('o', '.')
+    else:
+        password = password
+
+print('{x}q*s'.format(x=password))
