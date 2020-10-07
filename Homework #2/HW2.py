e3dc043af9b5feac990788ad9836c66af0a9f0a8
tran-rickydragon <70486087+tran-rickydragon@users.noreply.github.com>
2020-10-07 02:13:25 -0500

    Section C

--- Homework #2/HW2.py
+++ Homework #2/HW2.py
@@ -9,17 +9,19 @@ month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August
 months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
           'September': 9, 'October': 10, 'November': 11, 'December': 12}
 input_dates = open(r'C:\Users\ricky\OneDrive\Desktop\School\20\Fall_20\CIS2348\inputDates.txt')
+parsed_dates = open('C:\\Users\\ricky\\OneDrive\\Desktop\\School\\20\\Fall_20\\CIS2348\\parsedDates.txt', 'w')
 
-"""Modified the loop so that the input is taken from the file"""
+"""Modified the loop so that the input is taken from the file and now it will write outputs to the parsedDates"""
 
 for date in input_dates:
     x = date.split(' ')
     if x[0] in month:
         if ',' in date:
-                a = x[0]
-                b = x[1]
-                c = x[2]
-                a = months[a]
-                b = b.replace(',', '')
-                new_format = '{}/{}/{}'
-                print(new_format.format(a, b, c))
+            a = x[0]
+            b = x[1]
+            c = x[2]
+            a = months[a]
+            b = b.replace(',', '')
+            new_format = '{}/{}/{}'
+            parsed_dates.write(new_format.format(a, b, c))
+            print(new_format.format(a, b, c))
